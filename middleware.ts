d9c3e457c78db1bbc16b0e1378cd42d27cbbfd84
MohamedElsayed002 <mohamed.madoo2016@gmail.com>
2026-02-02 16:32:18 +0200

    logo

--- middleware.ts
+++ middleware.ts
@@ -36,7 +36,7 @@ export function middleware(req: NextRequest) {
 
 export const config = {
     matcher: [
-        "/((?!api|_next|_vercel).*)",
+        "/((?!api|_next|_vercel|public).*)",
     ],
 }
 
